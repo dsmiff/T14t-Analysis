cbdff11903595fdce1c40322eccdc3624b60b832
Dominic Smith <dominic.smith@bristol.ac.uk>
2013-11-27 13:31:41 +0000

    nothing changed

--- Plotting.C
+++ Plotting.C
@@ -6,14 +6,14 @@ void Plotting()
 
   gROOT->Reset()
 
-  TCanvas *c0 = new TCanvas("plots","MET", 600, 780);
+  TCanvas *c0 = new TCanvas("plots","HT_1000", 600, 780);
   TLegend *leg = new TLegend(0.5,0.8,0.9,0.88);
   
   gStyle->SetOptStat(0);
   gStyle->SetTitleFontSize(0.06);
 
   TString dirname = ".";
-  TString rootname = "MET_pyth_500_100.root";
+  TString rootname = "HT_1000_500_100.root";
   rootname = dirname + "/" + rootname;
   delete gROOT->GetListOfFiles()->FindObject(rootname);
 
@@ -24,18 +24,21 @@ void Plotting()
   rootfile->GetListOfKeys()->Print();
   rootfile->ls();
 
-  TH1F *h1  = (TH1F*)rootfile->Get("MET_histo");
+  TH1F *h1  = (TH1F*)rootfile->Get("HT");
   c0->cd();
   h1->Draw("hist");
-  h1->SetTitle("MET > 5 GeV, as defined by Delphes");
-  h1->GetXaxis()->SetTitle("MET");
+  h1->SetTitle("HT as constructed manually");
+  h1->GetXaxis()->SetTitle("HT");
   h1->GetYaxis()->SetTitle("Entries");
+  h1->GetXaxis()->SetRangeUser(0,2000);
+  h1->GetYaxis()->SetRangeUser(0,2000);
   // h1->Rebin(); // Only if neccessary
 
   Double_t entries = h1->GetEntries();
   std::cout << "# entries for histo: " << entries << std::endl;
 
-  leg->AddEntry(h1, "MET for m_{stop} = 500 GeV, m_{lsp} = 100 GeV", "l");
+  leg->AddEntry(h1, "HT for m_{g} = 1 TeV, m_{stop} = 500 GeV, m_{lsp} = 100 GeV", "l");
+  leg->AddEntry((TObject*)0,"HT = #sum^{Njets} E_{T}","");
   leg->Draw();
   leg->SetFillStyle(0);
   leg->SetBorderSize(0);
