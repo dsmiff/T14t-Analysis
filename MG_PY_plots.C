41ebdbd850f88f72486e65d7a5c719119275044a
Dominic Smith <dominic.smith@bristol.ac.uk>
2013-12-03 16:23:43 +0000

    nothing different

--- MG_PY_plots.C
+++ MG_PY_plots.C
@@ -9,8 +9,8 @@ void MG_PY_plots()
   TCanvas *c0 = new TCanvas("MGvsPY","MGvsPY", 600, 780);
 
   TString dirname = ".";
-  TString rootname1 = "PT_pyth_500_100.root";
-  TString rootname2 = "PT_500_100.root";
+  TString rootname1 = "PT_mg1.root";
+  TString rootname2 = "PT_mg1_prep.root";
   rootname1 = dirname + "/" + rootname1;
   rootname2 = dirname + "/" + rootname2;
   delete gROOT->GetListOfFiles()->FindObject(rootname1);
@@ -30,22 +30,18 @@ void MG_PY_plots()
 
   TH1F *h1 = (TH1F*)rootfile->Get("Top_Stop");
   h1->Draw();
-  Double_t norm_factor1 = h1->GetIntegral();
   Double_t entries1 = h1->GetEntries();
-  std::cout << "NORM: " << norm_factor1 << std::endl;
   std::cout << "# Entries for Pythia generated histo: " << entries1 << std::endl;
-  h1->Scale(4/norm_factor1);
+  h1->Scale(1/entries1);
   h1->Draw();
 
 
   TH1F *h2 = (TH1F*)rootfile2->Get("Top_Stop");
   h2->Draw("SAME");
   h2->SetLineColor(6);
-  Double_t norm_factor2 = h2->GetIntegral();
   Double_t entries2 = h2->GetEntries();
-  std::cout << "NORM2: " << norm_factor2 << std::endl;
   std::cout << "# Entries for MadGraph generated histo: " << entries2 << std::endl;
-  h2->Scale(1/norm_factor1);
+  h2->Scale(1/entries2);
   h2->Draw("SAME");
 
 
