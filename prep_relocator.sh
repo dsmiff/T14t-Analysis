5bff70e87e1405bd7207babfc88252ef95b7513a
Dominic Smith <dominic.smith@bristol.ac.uk>
2014-02-26 14:13:40 +0000

    yup

---
+++ prep_relocator.sh
@@ -0,0 +1,29 @@
+####################
+####  Macro to move PREP / MG root files to another directory for analysis
+####  Author :  Dominic Smith, University of Bristol
+###   Date : 26/02/2014
+####################
+
+Comp_dir=../Comparisons/
+
+if [ -d "$Comp_dir" ]; then
+    echo $Comp_dir already exists
+fi    
+
+
+echo Finding your files...
+
+
+ls | grep "PREP" > FILES.txt
+
+
+
+cp `cat FILES.txt` $Comp_dir
+
+echo Files copied to Comparisons directory
+
+cd $Comp_dir
+mkdir PREP
+ls | grep "PREP" > FILES.txt
+
+cp `cat FILES.txt` PREP/
