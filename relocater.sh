2e1dc3150d3250d6396e0a8aec7de9b4dd868ca8
Dominic Smith <dominic.smith@bristol.ac.uk>
2014-02-26 17:05:00 +0000

    blurp

--- relocater.sh
+++ relocater.sh
@@ -14,12 +14,19 @@ fi
 echo Finding your files...
 
 
-ls | grep "PREP" > FILES.txt
+ls | grep "MG5" > MG5FILES.txt
 
 
 
-cp `cat FILES.txt` $Comp_dir
+cp `cat MG5FILES.txt` $Comp_dir
 
 echo Files copied to Comparisons directory
 
 cd $Comp_dir
+mkdir MG5
+ls | grep "MG5" > MG5FILES.txt
+
+cp `cat MG5FILES.txt` MG5/
+rm `cat MG5FILES.txt`
+cd MG5
+rm MG5FILES.txt
