82d9af03efe12f5f9708cfc59f05f31c36452243
Dominic Smith <dominic.smith@bristol.ac.uk>
2014-03-04 14:11:47 +0000

    preparing changes

--- MyClass.h
+++ MyClass.h
@@ -539,9 +539,9 @@ MyClass::MyClass(TTree *tree) : fChain(0)
 // if parameter tree is not specified (or zero), connect the file
 // used to generate this class and read the Tree.
    if (tree == 0) {
-      TFile *f = (TFile*)gROOT->GetListOfFiles()->FindObject("../Comparisons/MG5/tag_1_delphes_events.root");
+      TFile *f = (TFile*)gROOT->GetListOfFiles()->FindObject("../Comparisons/MG5/tag_2_delphes_events.root");
       if (!f || !f->IsOpen()) {
-         f = new TFile("../Comparisons/MG5/tag_1_delphes_events.root");
+         f = new TFile("../Comparisons/MG5/tag_2_delphes_events.root");
       }
       f->GetObject("Delphes",tree);
 
